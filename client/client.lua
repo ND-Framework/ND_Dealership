732428e209046f8eb8fdad53d49a646bf06b1fdd
Lucas <genevasterm@protonmail.ch>
2022-10-31 01:48:34 -0500

    tweak: misc changes

--- client/client.lua
+++ client/client.lua
@@ -1,61 +1,61 @@
-local workerLocation = vec4(-33.67, -1100.36, 26.42, 78.64)
+local workerLocation = vec4(-33.17, -1100.59, 26.42, 68.77)
 local worker = 0
 local dealerShown = false
 local pedCoords = vec3(0, 0, 0)
-local newCam = 0
-local vehicle = 0
+local nearDealer = false
+local cam = 0
+local displayVehicle = 0
+local cellphone = 0
 
---- Creates and returns a camera.
----@param view string (left, right)
----@return number cam
-local function changeCamera(view)
-    local offset = GetOffsetFromEntityInWorldCoords(vehicle, 0.0, 6.0, 0.0)
-
-    if view == 'left' then local cam = CreateCameraWithParams('DEFAULT_SCRIPTED_CAMERA', offset.x, offset.y - 5.0, offset.z + 0.8, 0.0, 0.0, 0.0, 30.0, false, 0) return cam end
-
-    local cam = CreateCameraWithParams('DEFAULT_SCRIPTED_CAMERA', offset.x, offset.y + 5.0, offset.z + 0.8, 0.0, 0.0, 0.0, 30.0, false, 0)
-
-    return cam
-end
-
-local function createVehicleCam(model)
+local function createVehicleCam(model, price)
     if not IsModelAVehicle(model) or not IsModelInCdimage(model) then return end
 
     lib.requestModel(model)
 
-    vehicle = CreateVehicle(model, -44.38, -1098.05, 26.42, 255.33, false, false)
-    local offset = GetOffsetFromEntityInWorldCoords(vehicle, 0.0, 6.0, 0.0)
-    local cam = CreateCameraWithParams('DEFAULT_SCRIPTED_CAMERA', offset.x, offset.y + 1.5, offset.z+ 0.8, 0.0, 0.0, 0.0, 30.0, false, 0)
+    displayVehicle = CreateVehicle(model, -44.38, -1098.05, 26.42, 248.96, false, false)
+    repeat Wait(0) until DoesEntityExist(displayVehicle)
+    SetVehicleNumberPlateText(displayVehicle, 'DEALER')
+    SetVehicleNumberPlateTextIndex(displayVehicle, 4)
+    SetModelAsNoLongerNeeded(model)
 
+    local offset = GetOffsetFromEntityInWorldCoords(displayVehicle, 0.0, 6.0, 0.0)
+    cam = CreateCameraWithParams('DEFAULT_SCRIPTED_CAMERA', offset.x, offset.y, offset.z + 0.8, 0.0, 0.0, 0.0, 30.0, false, 0)
     SetCamActive(cam, true)
-    RenderScriptCams(true, true, 500, true, true)
     PointCamAtCoord(cam, -44.38, -1098.05, 26.42)
+    RenderScriptCams(true, true, 500, true, true)
 
-    while IsCamActive(cam) or IsCamActive(newCam) do
+    while IsCamActive(cam) do
         Wait(0)
+
+        lib.showTextUI('($' .. price .. ') ' .. GetLabelText(GetDisplayNameFromVehicleModel(model)), {
+            position = 'top-center',
+            icon = 'car'
+        })
 
         -- S key
         if IsControlJustPressed(0, 8) then
-            newCam = cam
-            SetCamActive(newCam, true)
+            cam = CreateCameraWithParams('DEFAULT_SCRIPTED_CAMERA', offset.x, offset.y, offset.z + 0.8, 0.0, 0.0, 0.0, 30.0, false, 0)
+            SetCamActive(cam, true)
+            PointCamAtCoord(cam, -44.38, -1098.05, 26.42)
             RenderScriptCams(true, true, 500, true, true)
-            PointCamAtCoord(newCam, -44.38, -1098.05, 26.42)
+            PointCamAtCoord(cam, -44.38, -1098.05, 26.42)
+            RenderScriptCams(true, true, 500, true, true)
         end
 
         -- D key
         if IsControlJustPressed(0, 9) then
-            newCam = changeCamera('right')
-            SetCamActive(newCam, true)
+            cam = CreateCameraWithParams('DEFAULT_SCRIPTED_CAMERA', offset.x, offset.y + 5.0, offset.z + 0.8, 0.0, 0.0, 0.0, 30.0, false, 0)
+            SetCamActive(cam, true)
+            PointCamAtCoord(cam, -44.38, -1098.05, 26.42)
             RenderScriptCams(true, true, 500, true, true)
-            PointCamAtCoord(newCam, -44.38, -1098.05, 26.42)
         end
 
         -- A key
         if IsControlJustPressed(0, 34) then
-            newCam = changeCamera('left')
-            SetCamActive(newCam, true)
+            cam = CreateCameraWithParams('DEFAULT_SCRIPTED_CAMERA', offset.x, offset.y - 3.8, offset.z + 0.8, 0.0, 0.0, 0.0, 30.0, false, 0)
+            SetCamActive(cam, true)
+            PointCamAtCoord(cam, -44.38, -1098.05, 26.42)
             RenderScriptCams(true, true, 500, true, true)
-            PointCamAtCoord(newCam, -44.38, -1098.05, 26.42)
         end
 
         -- E key
@@ -63,10 +63,12 @@ local function createVehicleCam(model)
             SetCamActive(cam, false)
             RenderScriptCams(false, true, 500, true, true)
             DestroyCam(cam, false)
-            DeleteVehicle(vehicle)
-            break
+            DeleteVehicle(displayVehicle)
         end
     end
+
+    dealerShown = false
+    lib.hideTextUI()
 end
 
 AddEventHandler('onResourceStart', function(resourceName)
@@ -109,23 +111,20 @@ AddEventHandler('onResourceStart', function(resourceName)
     }, function(_, scrollIndex, _)
         for i = 1, #Config.vehicles.compacts do
             if i == scrollIndex then
-                lib.defaultNotify({
-                    title = 'Vehicle Selected',
-                    description = 'You are viewing the ' .. GetLabelText(GetDisplayNameFromVehicleModel(Config.vehicles.compacts[i].model)) .. ', [A] to go left, [D] to go right, [S] to center (offset) camera, [E] to exit',
-                    icon = 'car',
-                    position = 'top',
-                    status = 'success',
-                    duration = '4000'
-                })
-                createVehicleCam(Config.vehicles.compacts[i].model)
+                lib.hideMenu()
+                createVehicleCam(Config.vehicles.compacts[i].model, Config.vehicles.compacts[i].price)
             end
         end
     end)
 
-    if worker == 0 then
-        worker = spawnWorker(workerLocation)
-        print('Spawning dealership worker: ' .. worker)
-    end
+    local blip = AddBlipForCoord(workerLocation.x, workerLocation.y, workerLocation.z)
+    SetBlipSprite(blip, 523)
+    SetBlipColour(blip, 3)
+    SetBlipScale(blip, 0.8)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString('Dealership')
+    EndTextCommandSetBlipName(blip)
 end)
 
 AddEventHandler('onResourceStop', function(resourceName)
@@ -136,6 +135,23 @@ AddEventHandler('onResourceStop', function(resourceName)
     if worker ~= 0 then
         print('Deleting dealership worker: ' .. worker)
         DeletePed(worker)
+    end
+
+    if displayVehicle ~= 0 then
+        print('Deleting display vehicle: ' .. displayVehicle)
+        DeleteVehicle(displayVehicle)
+    end
+
+    if cam ~= 0 then
+        print('Destroying camera' .. cam)
+        SetCamActive(cam, false)
+        RenderScriptCams(false, true, 500, true, true)
+        DestroyCam(cam, false)
+    end
+
+    if cellphone ~= 0 then
+        print('Deleting cellphone: ' .. cellphone)
+        DeleteObject(cellphone)
     end
 end)
 
@@ -150,24 +166,42 @@ CreateThread(function()
     local sleep = 0
 
     while true do
-        if #(pedCoords - vec3(workerLocation.x, workerLocation.y, workerLocation.z)) <= 1.5 then
-            sleep = 0
+        nearDealer = false
+        local dist = #(pedCoords - vec3(workerLocation.x, workerLocation.y, workerLocation.z))
 
-            if not dealerShown then
-                lib.showTextUI('[E] - Open Dealer Menu', {
-                    icon = 'car'
-                })
+        if dist <= 95.0 then
+            nearDealer = true
+
+            if worker == 0 then
+                worker, cellphone = spawnWorker(workerLocation)
+                print('Spawning dealership worker: ' .. worker)
+            end
+
+            if dist < 1.5 then
+                sleep = 0
+
+                if not dealerShown then
+                    lib.showTextUI('[E] - Open Dealer Menu', {
+                        icon = 'car'
+                    })
+                else
+                    lib.hideTextUI()
+                end
+
+                if not dealerShown and IsControlJustPressed(0, 54) then
+                    dealerShown = true
+                    lib.showMenu('dealer_menu')
+                end
             else
                 lib.hideTextUI()
+                sleep = 500
             end
+        end
 
-            if IsControlJustPressed(0, 54) then
-                dealerShown = true
-                lib.showMenu('dealer_menu')
-            end
-        else
-            lib.hideTextUI()
-            sleep = 1500
+        if not nearDealer and worker ~= 0 then
+            print('Deleting dealership worker: ' .. worker)
+            DeletePed(worker)
+            worker = 0
         end
 
         Wait(sleep)
