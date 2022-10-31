ce8643f3a805feb81de6de6205462a1800877196
Lucas <genevasterm@protonmail.ch>
2022-10-31 01:56:13 -0500

    tweak: cleanup memory

--- client/functions.lua
+++ client/functions.lua
@@ -24,5 +24,8 @@ function spawnWorker(location)
     local cellphone = CreateObject(`prop_v_m_phone_01`, 0.0, 0.0, 0.0, true, false, false)
     AttachEntityToEntity(cellphone, worker, bone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, true, true, false, false, 2, true)
 
+    SetModelAsNoLongerNeeded(`cs_siemonyetarian`)
+    SetModelAsNoLongerNeeded(`prop_v_m_phone_01`)
+
     return worker, cellphone
 end
